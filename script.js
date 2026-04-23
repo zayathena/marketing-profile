28e951f1407d69c49428f8635f040dee7d39df0b
Clare Regan <claremaryregan@outlook.com>
2026-04-23 17:51:36 +0200

    fixing overall styling

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       entry.target.classList.add('show');
+      entry.target.classList.remove('hidden');
     }
   });
+}, {
+  threshold: 0.2
 });
 
 hiddenElements.forEach((el) => observer.observe(el));
