8b18c3f9067e9391633ec06514c34848f9e4b945
Clare Regan <claremaryregan@outlook.com>
2026-04-23 16:16:28 +0200

    added js file

---
+++ script.js
@@ -0,0 +1,11 @@
+const hiddenElements = document.querySelectorAll('.hidden');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+});
+
+hiddenElements.forEach((el) => observer.observe(el));
